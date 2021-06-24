d1814149f0c02d3c78ad31900f126c74d3a9f959
Akansha0211 <akansha.saxena0211@gmail.com>
2021-06-24 23:10:17 +0530

    Working on layout of frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {Container,AppBar,Typography, Grow, Grid} from '@material-ui/core'
+import blogger from './images/teamwork.png';
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth ="lg">
+      <AppBar position = "static" color="inherit">
+        <Typography variant="h2" align="center">Posts</Typography>
+        <img src={blogger} alt="posts" height="60" width="65"/>
+      </AppBar>
+    </Container>
   );
 }
 
