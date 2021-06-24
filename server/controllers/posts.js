a13ef65aa5304dcf076379030b96b0df613de4ee
Akansha0211 <akansha.saxena0211@gmail.com>
2021-06-24 21:10:10 +0530

    Get all posts in the database

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,12 @@
-export const getPosts = (req,res)=>{
-    res.send("THIS WORKS");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts =  async(req,res)=>{
+    try{
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
+    }catch(error){
+        res.status(404).json({message : error.message})
+    }
 }
 export const createPosts = (req,res)=>{
     res.send("Post created")
