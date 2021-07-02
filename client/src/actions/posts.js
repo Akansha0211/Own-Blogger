20c89d6e6fad78b5d4625fa5ce6364f8dec27242
Akansha0211 <akansha.saxena0211@gmail.com>
2021-07-02 23:12:30 +0530

    Succesfully used redux to pass data from backend

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,8 +1,14 @@
 import * as api from '../api';
 
 // Actions creators
-const getPosts = () => async(dispatch) => {
-    const action = {type:'FETCH_ALL', payload:[]}
+export const getPosts = () => async(dispatch) => {
+
+    try{
+        const {data} = await api.fetchPosts();
+        dispatch({type:'FETCH_ALL', payload: data});
+    }catch(error){
+        console.log(error.message);
+    }
 
     dipatch(action);
 }
